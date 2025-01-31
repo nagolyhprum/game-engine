a09f7eb21642141fc5ba7bf7f35a2b88f8af0c5b
Logan Murphy <nagolyhprum@hotmail.com>
2025-01-31 11:56:19 -0500

    start with minesweeper

---
+++ scripts/minesweeper.ts
@@ -0,0 +1,57 @@
+const MENU_HEIGHT = 50;
+const CELL_SIZE = 40;
+const ROWS = 10;
+const COLUMNS = 15;
+const WIDTH = CELL_SIZE * COLUMNS;
+const HEIGHT = MENU_HEIGHT + CELL_SIZE * ROWS;
+
+const canvas = document.querySelector("canvas")!;
+const context = canvas.getContext("2d")!;
+
+canvas.width = WIDTH;
+canvas.height = HEIGHT;
+
+// cells have a danger or not
+// cells are flagged or not
+// cells are flipped or not
+
+const state = {
+  cells: Array.from({ length: ROWS }).map(() =>
+    Array.from({ length: COLUMNS }).map(() => ({
+      isFlagged: false,
+      isRevealed: false,
+      isDangerous: false,
+      surroundingDangers: 0,
+    }))
+  ),
+};
+
+state.cells[0][0].isFlagged = true;
+state.cells[0][1].isDangerous = true;
+state.cells[0][2].isRevealed = true;
+
+const clear = () => {
+  context.fillStyle = "lightgray";
+  context.fillRect(0, 0, WIDTH, HEIGHT);
+};
+
+const drawCells = () => {
+  context.beginPath();
+  for (let row = 0; row < ROWS; row++) {
+    context.moveTo(0, row * CELL_SIZE + MENU_HEIGHT);
+    context.lineTo(0 + WIDTH, row * CELL_SIZE + MENU_HEIGHT);
+  }
+  for (let column = 1; column < COLUMNS; column++) {
+    context.moveTo(column * CELL_SIZE, MENU_HEIGHT);
+    context.lineTo(column * CELL_SIZE, HEIGHT);
+  }
+  context.strokeStyle = "darkgray";
+  context.stroke();
+};
+
+const draw = () => {
+  clear();
+  drawCells();
+};
+
+draw();
---
+++ src/index.ts
@@ -0,0 +1,75 @@
+import fs from "fs/promises";
+import path from "path";
+import { URL } from "url";
+
+const port = process.env.PORT || 80;
+
+const html = `<!doctype html>
+<html>
+    <head>
+      <style>
+        canvas {
+          background : black;
+        }
+      </style>
+    </head>
+    </body>
+        <canvas>Canvas element not supported.</canvas>
+        <img width="300" height="300" src="/public/minesweeper-inspiration.png" />
+        <script src="/scripts/minesweeper"></script>
+    </body>
+</html>`;
+
+const PUBLIC = "/public/";
+const SCRIPTS = "/scripts/";
+
+const scriptsRouter = new Bun.FileSystemRouter({
+  dir: path.join(__dirname, "..", "scripts"),
+  style: "nextjs",
+});
+
+const extensions: Record<string, string> = {
+  ".png": "image/png",
+};
+
+Bun.serve({
+  async fetch(request) {
+    const url = new URL(request.url);
+    if (url.pathname.startsWith(PUBLIC)) {
+      const filePath = path.join(__dirname, "..", url.pathname);
+      const extension = path.extname(filePath);
+      const file = await fs.readFile(filePath);
+      return new Response(file, {
+        headers: {
+          "Content-Type": extensions[extension],
+        },
+      });
+    }
+    if (url.pathname.startsWith(SCRIPTS)) {
+      const match = scriptsRouter.match(url.pathname.slice(SCRIPTS.length - 1));
+      if (match) {
+        const output = await Bun.build({
+          entrypoints: [match.filePath],
+          minify: true,
+        });
+        console.log(output);
+        const body = await Promise.all(
+          output.outputs.map((output) => output.text())
+        );
+        return new Response(body.join(""), {
+          headers: {
+            "Content-Type": "text/javascript",
+          },
+        });
+      }
+    }
+    return new Response(html, {
+      headers: {
+        "Content-Type": "text/html",
+      },
+    });
+  },
+  port,
+});
+
+console.log(`Running on port ${port}`);
